7dcb6117a888f703a89092d37ea3850c285e6085
parkjunsoo <01junsoo@gmail.com>
2022-02-24 09:23:44 +0900

    feat: vanilla study

---
+++ vanillajs-spa/App.js
@@ -0,0 +1,12 @@
+function App($target) {
+  const { pathname } = location;
+  console.log(pathname);
+  const init = () => {};
+  const render = () => {
+    console.log("@@@@@@");
+  };
+
+  render();
+}
+
+export default App;

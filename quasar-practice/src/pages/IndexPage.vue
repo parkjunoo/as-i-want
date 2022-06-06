e20a6b64bd18a0e5ca918d399dffdc5c6ce0b07e
parkjunsoo <01junsoo@gmail.com>
2022-06-06 22:48:18 +0900

    feat: todo list

--- quasar-practice/src/pages/IndexPage.vue
+++ quasar-practice/src/pages/IndexPage.vue
@@ -1,49 +1,31 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <q-header class="items-center" v-model="header" reveal elevated
+      >Todo
+    </q-header>
+    <q-card class="my-card">
+      <div class="todo-title">TodoList Practice</div>
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+      <q-card-section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { Todo, Meta } from '../components/models';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
   setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
+    return {};
+  },
 });
 </script>
+
+<style lang="sass" scoped></style>
